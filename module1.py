66eba4284aee6ef0bbfc33b071c76a58b11becc2
jesus10005 <jesus.gomez.10005@gmail.com>
2020-04-18 22:07:12 +0200

    add printf

---
+++ module1.py
@@ -0,0 +1,32 @@
+'''
+Created on 16 abr. 2020
+
+@author: gomezj
+'''
+
+''' if __name__ == '__main__':
+'    pass
+'''
+
+import random
+
+print('hello world')
+
+# tipo cadena
+str='esto es una cadena'
+# tipo numero
+num1=123
+
+# type(str)
+# type(num1)
+
+' kkkkk
+
+def function(a, b):
+    print (a, b)
+ 
+function(5, 6)
+
+function('aaaaaaaa', 'BBBBBBBBB')
+
+function ('first', 'second')
